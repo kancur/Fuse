7db192b661f59caefa9e11e09a993a3eb2573cc3
Peter Smid <petosmid@gmail.com>
2021-10-17 11:42:59 +0200

    custom use case test

---
+++ test-zero.js
@@ -0,0 +1,35 @@
+const Fuse = require('./dist/fuse')
+
+const options = {
+  // isCaseSensitive: false,
+  includeScore: true,
+  // shouldSort: true,
+  // includeMatches: false,
+  findAllMatches: true,
+  // minMatchCharLength: 1,
+  // location: 0,
+  threshold: 0.3,
+  // distance: 100,
+  // useExtendedSearch: false,
+  // ignoreLocation: false,
+  // ignoreFieldNorm: false,
+  keys: ['title']
+}
+
+const list = [
+  { title: 'kremnica' },
+  { title: 'kremnica' },
+  { title: 'kremnicak' },
+  { title: 'kremnicakasdfgjkagdnag' },
+  { title: 'kremnica je super' },
+  { title: 'kremnic' },
+  { title: 'random' }
+]
+
+const fuse = new Fuse(list, options)
+
+// Change the pattern
+const pattern = 'kremnica'
+console.log('QUERY ==========>', pattern)
+
+console.log(fuse.search(pattern))
